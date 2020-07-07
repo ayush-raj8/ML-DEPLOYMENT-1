a87dbe32230498a0b5784566b4ccada7d181820c
rayushbpgc <52033949+rayushbpgc@users.noreply.github.com>
2020-07-07 18:07:15 +0530

    Add files via upload

---
+++ request.py
@@ -0,0 +1,6 @@
+import requests
+
+url = 'http://localhost:5000/predict_api'
+r = requests.post(url,json={'experience':2, 'test_score(out of 10)':9, 'interview_score(out of 10)':6})
+
+print(r.json())
